5dad81d4ba34b38b3c022737ceabeb3053155295
DoRoy <doroy6@gmail.com>
2019-04-22 18:53:20 +0300

    lots of good stuff

---
+++ Board.js
@@ -0,0 +1,147 @@
+
+var EMPTY       = 0;
+var WALL        = 1;
+var PACMAN      = 2;
+var BISCUIT     = 3; // special power
+var BLOCK       = 4;
+var GHOST       = 5;
+var PILL_5      = 105;
+var PILL_15     = 115;
+var PILL_25     = 125;
+
+Board = function() {
+    var board;
+    function initBoard() {
+        board_demo = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+            [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+            [4, 4, 4, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 4, 4, 4],
+            [1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1],
+            [4, 4, 4, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 4, 4, 4],
+            [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+            [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ];
+        board = new Array();
+        for (var i = 0; i < board_demo[0].length; i++){
+            board[i] = new Array();
+            for (var j = 0; j < board_demo.length; j++){
+                board[i][j] = board_demo[j][i];
+            }
+        }
+
+        var food_remain = pill_number;
+        // start_time = new Date();
+        // Assign PACMAN location
+        // var emptyCellPacman = findRandomEmptyCell(board);
+        // board[emptyCellPacman[0]][emptyCellPacman[1]] = PACMAN;
+        // shape.i = emptyCellPacman[0];
+        // shape.j = emptyCellPacman[1];
+        // Assign all PILLS
+        var pill_5_number = Math.floor(pill_number * 0.6);
+        var pill_15_number = Math.floor(pill_number * 0.3);
+        var pill_25_number = Math.floor(pill_number * 0.1);
+        while (pill_15_number > 0){
+            var emptyCell = findRandomEmptyCell();
+            board[emptyCell[0]][emptyCell[1]] = PILL_15;
+            food_remain--;
+            pill_15_number--;
+        }
+        while (pill_25_number > 0){
+            var emptyCell = findRandomEmptyCell();
+            board[emptyCell[0]][emptyCell[1]] = PILL_25;
+            food_remain--;
+            pill_25_number--;
+        }
+        while (food_remain > 0) {
+            var emptyCell = findRandomEmptyCell();
+            board[emptyCell[0]][emptyCell[1]] = PILL_5;
+            food_remain--;
+        }
+    }
+
+    function findRandomEmptyCell() {
+        var i = Math.floor((Math.random() * board.length));
+        var j = Math.floor((Math.random() * board[0].length) );
+        while (board[i][j] !== EMPTY || (j === 10 && (i >= 8 || i <= 10)) || (j === 9 && i === 9)) {
+            i = Math.floor((Math.random() * board.length));
+            j = Math.floor((Math.random() * board[0].length) );
+        }
+        return [i, j];
+    }
+
+    function getBoard(){
+        return board;
+    }
+
+    function boardAt(i, j){
+        try {
+            return board[i][j];
+        }
+        catch (e) {
+            console.log(e.message);
+            return WALL;
+        }
+    }
+
+    function getPossibleMoves(i, j){
+        moves = new Array();
+        if (i - 1 >= 0 && (boardAt(i-1,j) !== WALL && boardAt(i-1,j) !== BLOCK)){
+            moves.push({'i': i-1,
+                        'j': j});
+        }else if (i - 1 < 0 && (boardAt(board.length - 1,j) !== WALL && boardAt(board.length - 1,j) !== BLOCK)){
+            moves.push({'i': board.length - 1,
+                'j': j});
+        }
+        if (j - 1 >= 0 && (boardAt(i,j-1) !== WALL && boardAt(i,j-1) !== BLOCK)){
+            moves.push({'i': i,
+                            'j': j-1});
+        }
+        if (i + 1 < board.length - 1 && (boardAt(i+1,j) !== WALL && boardAt(i+1,j) !== BLOCK)){
+            moves.push({'i': i+1,
+                'j': j});
+        }else if (i + 1 === board.length && (boardAt(0,j) !== WALL && boardAt(0,j) !== BLOCK)){
+            moves.push({'i': 0,
+                'j': j});
+        }
+        if (j + 1 < board[0].length && (boardAt(i,j+1) !== WALL && boardAt(i,j+1) !== BLOCK)){
+            moves.push({'i': i,
+                            'j': j+1});
+        }
+
+        if (j - 1 < 0 && (boardAt(i,j-1) !== WALL && boardAt(i,j-1) !== BLOCK)){
+            moves.push({'i': i,
+                'j': j-1});
+        }
+        if (i + 1 === board[0].length && (boardAt(i+1,j) !== WALL && boardAt(i+1,j) !== BLOCK)){
+            moves.push({'i': i+1,
+                'j': j});
+        }
+
+        return moves;
+
+    }
+
+    return {
+        'initBoard': initBoard,
+        'findRandomEmptyCell': findRandomEmptyCell,
+        'getBoard': getBoard,
+        'boardAt': boardAt,
+        'getPossibleMoves': getPossibleMoves
+    };
+
+};
--- GameLogic.js
+++ GameLogic.js
@@ -9,89 +9,51 @@ var PILL_5      = 105;
 var PILL_15     = 115;
 var PILL_25     = 125;
 
+
+var NONE        = 4,
+    UP          = 3,
+    LEFT        = 2,
+    DOWN        = 1,
+    RIGHT       = 11,
+    WAITING     = 5,
+    PAUSE       = 6,
+    PLAYING     = 7,
+    COUNTDOWN   = 8,
+    EATEN_PAUSE = 9,
+    DYING       = 10;
+
+var  heightStep,
+     widthStep  ;
+
 var pacmanDirection = 'right';
 
+function getTick(){
+    return tick;
+}
 
 function Start() {
-    board = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
-        [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
-        [4, 4, 4, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 4, 4, 4],
-        [1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
-        [4, 4, 4, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 4, 4, 4],
-        [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
-        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-        [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
-        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ];
     score = 0;
+    tick = 0;
+    generateRandomStart();
     pac_color = "yellow";
-    var cnt = 100;
-    var food_remain = pill_number;
-    var pacman_remain = 1;
+    // var food_remain = pill_number;
     start_time = new Date();
     // Assign PACMAN location
-    var emptyCellPacman = findRandomEmptyCell(board);
-    board[emptyCellPacman[0]][emptyCellPacman[1]] = PACMAN;
-    shape.i = emptyCellPacman[0];
-    shape.j = emptyCellPacman[1];
-    // Assign all PILLS
-    var pill_5_number = Math.floor(pill_number * 0.6);
-    var pill_15_number = Math.floor(pill_number * 0.3);
-    var pill_25_number = Math.floor(pill_number * 0.1);
-    while (pill_15_number > 0){
-        var emptyCell = findRandomEmptyCell(board);
-        board[emptyCell[0]][emptyCell[1]] = PILL_15;
-        food_remain--;
-        pill_15_number--;
-    }
-    while (pill_25_number > 0){
-        var emptyCell = findRandomEmptyCell(board);
-        board[emptyCell[0]][emptyCell[1]] = PILL_25;
-        food_remain--;
-        pill_25_number--;
-    }
-    while (food_remain > 0) {
-        var emptyCell = findRandomEmptyCell(board);
-        board[emptyCell[0]][emptyCell[1]] = PILL_5;
-        food_remain--;
-    }
-    // for (var i = 0; i < 10; i++) {
-    //     // board[i] = new Array();
-    //     //put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-    //     for (var j = 0; j < 10; j++) {
-    //         if ((i === 3 && j === 3) || (i === 3 && j === 4) || (i === 3 && j === 5) || (i === 6 && j === 1) || (i === 6 && j === 2)) {
-    //             board[i][j] = 4;
-    //         } else {
-    //             var randomNum = Math.random();
-    //             if (randomNum <= 1.0 * food_remain / cnt) {
-    //                 food_remain--;
-    //                 board[i][j] = 1;
-    //             } else if (randomNum < 1.0 * (pacman_remain + food_remain) / cnt) {
-    //                 shape.i = i;
-    //                 shape.j = j;
-    //                 pacman_remain--;
-    //                 board[i][j] = 2;
-    //             } else {
-    //                 board[i][j] = 0;
-    //             }
-    //             cnt--;
-    //         }
-    //     }
+    board = Board();
+    board.initBoard();
+    heightStep = canvas.height / board.getBoard().length;
+    widthStep = canvas.width / board.getBoard()[0].length;
+
+    // var emptyCellPacman = board.findRandomEmptyCell();
+    // // board.board[emptyCellPacman[0]][emptyCellPacman[1]] = PACMAN;
+    // shape.i = emptyCellPacman[0];
+    // shape.j = emptyCellPacman[1];
+    // for (var i = 0; i < monster_number; i ++){
+    //     ghosts[i] = MyGhost({"getTick":getTick}, board,'black', i);
+    //     ghosts[i].reset();
     // }
+    specialSnack = SpecialSnack({"getTick":getTick}, board,'black', 3);
+    setCharactersLocations();
 
     keysDown = {};
     addEventListener("keydown", function (e) {
@@ -100,19 +62,29 @@ function Start() {
     addEventListener("keyup", function (e) {
         keysDown[e.code] = false;
     }, false);
+    if (interval !== null){
+        clearInterval(interval);
+    }
     interval = setInterval(UpdatePosition, 250);
 }
 
-
-function findRandomEmptyCell(board) {
-    var i = Math.floor((Math.random() * board.length));
-    var j = Math.floor((Math.random() * board[0].length) );
-    while (board[i][j] !== EMPTY) {
-        i = Math.floor((Math.random() * board.length));
-        j = Math.floor((Math.random() * board[0].length) );
+function setCharactersLocations(){
+    var emptyCellPacman = board.findRandomEmptyCell();
+    // board.board[emptyCellPacman[0]][emptyCellPacman[1]] = PACMAN;
+    shape.i = emptyCellPacman[0];
+    shape.j = emptyCellPacman[1];
+    oldPos = null;
+    ghosts = [];
+    for (var i = 0; i < monster_number; i ++){
+        ghosts[i] = MyGhost({"getTick":getTick}, board,'black', i);
+        ghosts[i].reset();
     }
-    return [i, j];
+    if(specialSnack.isAlive()){
+        specialSnack.reset();
+    }
 }
+
+
 
 /**
  * @return {number}
@@ -136,102 +108,280 @@ function Draw() {
     context.clearRect(0, 0, canvas.width, canvas.height); //clean board
     lblScore.value = score;
     lblTime.value = time_elapsed;
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < board[0].length; j++) {
+    heightStep = canvas.height / board.getBoard()[0].length;
+    widthStep = canvas.width / board.getBoard().length;
+    for (var i = 0; i < board.getBoard().length; i++) {
+        for (var j = 0; j < board.getBoard()[0].length; j++) {
             var center = new Object();
-            center.x = i * 30 + 20;
-            center.y = j * 30 + 20;
-            if (board[i][j] === PACMAN) {
+            center.x = i * widthStep + widthStep / 2;
+            center.y = j * heightStep + heightStep / 2;
+            if (i === shape.i && j === shape.j) {
                 context.beginPath();
-                context.arc(center.x, center.y, 15, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-                context.lineTo(center.x, center.y);
-                context.fillStyle = pac_color; //color
-                context.fill();
-                context.beginPath();
-                context.arc(center.x + 3, center.y - 12, 4, 0, 2 * Math.PI); // circle
+                if(pacmanDirection === 'up'){
+                    context.arc(center.x, center.y, widthStep / 2,  (1.50 +.15) % 2 * Math.PI, (1.50 +1.85) % 2 * Math.PI); // half circle
+                    context.lineTo(center.x, center.y);
+                    context.fillStyle = pac_color; //color
+                    context.fill();
+                    context.beginPath();
+                    context.arc(center.x -12, center.y -3, 4, 0, 2 * Math.PI); // circle
+                }else if (pacmanDirection === 'down'){
+                    context.arc(center.x, center.y, widthStep / 2, (0.5 + 0.15) % 2 * Math.PI, (0.5 + 1.85) % 2 * Math.PI); // half circle
+                    context.lineTo(center.x, center.y);
+                    context.fillStyle = pac_color; //color
+                    context.fill();
+                    context.beginPath();
+                    context.arc(center.x + 12, center.y +3, 4, 0, 2 * Math.PI); // circle
+                }else if(pacmanDirection === 'right'){
+                    context.arc(center.x, center.y, widthStep / 2, (0 + 0.15) % 2 * Math.PI, (0 + 1.85) % 2  * Math.PI); // half circle
+                    context.lineTo(center.x, center.y);
+                    context.fillStyle = pac_color; //color
+                    context.fill();
+                    context.beginPath();
+                    context.arc(center.x + 3, center.y - 12, 4, 0, 2 * Math.PI); // circle
+                }else if (pacmanDirection === 'left'){
+                    context.arc(center.x, center.y, widthStep / 2, (1 + 0.15) % 2* Math.PI, (1 + 1.85) % 2 * Math.PI); // half circle
+                    context.lineTo(center.x, center.y);
+                    context.fillStyle = pac_color; //color
+                    context.fill();
+                    context.beginPath();
+                    context.arc(center.x + 3, center.y - 12, 4, 0, 2 * Math.PI); // circle
+                }
                 context.fillStyle = "black"; //color
                 context.scale.x = -1;
                 // rotatePacman(context);
                 context.fill();
                 context.setTransform(1, 0, 0, 1, 0, 0);
-            } else if (board[i][j] > 100) {
+            }
+
+            else if (board.boardAt(i,j) > 100) {
                 context.beginPath();
-                context.arc(center.x, center.y, 8, 0, 2 * Math.PI); // circle
+                // context.font = '10px Helvetica';
+                // context.textAlign = 'center';
+                // context.textBaseline = 'middle';
+
                 // set color
-                if (board[i][j] === PILL_5){
+                if (board.boardAt(i,j) === PILL_5){
                     context.fillStyle = "purple"; //color
+                    context.arc(center.x, center.y, 10, 0, 2 * Math.PI); // circle
+                    context.fill();
+                    context.strokeStyle = '#ffffff';
+                    context.font = '10px Helvetica';
+                    context.textAlign = 'center';
+                    context.textBaseline = 'middle';
+                    context.fillStyle = 'white';
+                    context.fillText('5', center.x, center.y);
+                    context.stroke();
+                    context.closePath();
                 }
-                else if(board[i][j] === PILL_15){
+                else if(board.boardAt(i,j) === PILL_15){
                     context.fillStyle = "red"; //color
+                    context.arc(center.x, center.y, 10, 0, 2 * Math.PI); // circle
+                    context.fill();
+                    context.strokeStyle = '#ffffff';
+                    context.font = '10px Helvetica';
+                    context.textAlign = 'center';
+                    context.textBaseline = 'middle';
+                    context.fillStyle = 'white';
+                    context.fillText('15', center.x, center.y);
+                    context.stroke();
+                    context.closePath();
                 }else{
-                    context.fillStyle = "blue"; //color
+                    context.fillStyle = 'blue';
+                    context.arc(center.x, center.y, 10, 0, 2 * Math.PI); // circle
+                    context.fill();
+                    context.strokeStyle = '#ffffff';
+                    context.font = '10px Helvetica';
+                    context.textAlign = 'center';
+                    context.textBaseline = 'middle';
+                    context.fillStyle = 'white';
+                    context.fillText('25', center.x, center.y);
+                    context.stroke();
+                    context.closePath();
+
+
                 }
                 // context.fillStyle = "black"; //color
-                context.fill();
-            } else if (board[i][j] === WALL) {
+                // context.fill();
+
+            }
+            else if (board.boardAt(i,j) === WALL) {
                 context.beginPath();
-                context.rect(center.x - 20, center.y - 20, 30, 30);
+                context.rect(center.x - widthStep/2, center.y - heightStep/2, widthStep, heightStep);
                 context.fillStyle = "grey"; //color
                 context.fill();
             }
+
         }
     }
+    for (var i = 0; i < ghosts.length; i++){
+        ghosts[i].draw(context);
+    }
+    if (specialSnack.isAlive()){
+        specialSnack.draw(context);
+    }
 
 
 }
 
-function rotatePacman(ctx){
-    if(pacmanDirection === 'up'){
-        ctx.rotate(270 * Math.PI / 180);
-    }else if (pacmanDirection === 'down'){
-        ctx.rotate(90 * Math.PI / 180);
-    }else if(pacmanDirection === 'right'){
-
-    }else if (pacmanDirection === 'left'){
-        ctx.rotate(180 * Math.PI / 180);
+function moveGhosts(){
+    for(var i = 0; i < ghosts.length; i ++){
+        var possibleMoves = ghosts[i].getPossibleMoves();
+        var bestMoves = new Array();
+        var minDistance = 10000;
+        var chosenMove ;
+        // find minimum distance
+        for ( var j = 0; j < possibleMoves.length; j++){
+            if ( Math.abs(possibleMoves[j].i - shape.i) + Math.abs(possibleMoves[j].j - shape.j) <= minDistance){
+                minDistance = Math.abs(possibleMoves[j].i - shape.i) + Math.abs(possibleMoves[j].j - shape.j);
+            }
+        }
+        // take all the moves with minimum distance
+        for ( var j = 0; j < possibleMoves.length; j++){
+            if ( Math.abs(possibleMoves[j].i - shape.i) + Math.abs(possibleMoves[j].j - shape.j) === minDistance){
+                bestMoves.push(possibleMoves[j]);
+            }
+        }
+        chosenMove = bestMoves[Math.floor(Math.random() * bestMoves.length)];
+        ghosts[i].setPosition(chosenMove);
     }
 }
 
+
+function moveSpecialSnack(){
+    var possibleMoves = specialSnack.getPossibleMoves();
+    // var index = Math.floor(Math.random() * (possibleMoves.length));
+    // var chosenMove = possibleMoves[index];
+    // specialSnack.setPosition(chosenMove);
+
+
+    var bestMoves = new Array();
+    var minDistance = 10000;
+    var chosenMove ;
+    for ( var j = 0; j < possibleMoves.length; j++){
+        if ( Math.abs(possibleMoves[j].i - shape.i) + Math.abs(possibleMoves[j].j - shape.j) <= minDistance){
+            minDistance = Math.abs(possibleMoves[j].i - shape.i) + Math.abs(possibleMoves[j].j - shape.j);
+        }
+    }
+    // take all the moves with minimum distance
+    for ( var j = 0; j < possibleMoves.length; j++){
+        if ( Math.abs(possibleMoves[j].i - shape.i) + Math.abs(possibleMoves[j].j - shape.j) === minDistance){
+            bestMoves.push(possibleMoves[j]);
+        }
+    }
+    chosenMove = bestMoves[Math.floor(Math.random() * bestMoves.length)];
+    specialSnack.setPosition(chosenMove);
+
+}
+
+
 function UpdatePosition() {
-    board[shape.i][shape.j] = 0;
+    board.getBoard()[shape.i][shape.j] = EMPTY;
+
+    tick++;
+
     var x = GetKeyPressed();
+    moveGhosts();
+    if (specialSnack.isAlive()) {
+        moveSpecialSnack();
+    }
     if (x === 1) {
-        if (shape.j > 0 && board[shape.i][shape.j - 1] !== WALL) {
+        if (shape.j > 0 && board.boardAt(shape.i, shape.j - 1) !== WALL) {
+            oldPos = myClone(shape);
             shape.j--;
         }
         pacmanDirection = 'up';
     }
     if (x === 2) {
-        if (shape.j < 9 && board[shape.i][shape.j + 1] !== WALL) {
+        if (shape.j < board.getBoard()[0].length && board.boardAt(shape.i, shape.j + 1) !== WALL) {
+            oldPos = myClone(shape);
             shape.j++;
         }
         pacmanDirection = 'down';
     }
     if (x === 3) {
-        if (shape.i > 0 && board[shape.i - 1][shape.j] !== WALL) {
+        if (shape.i > 0 && board.boardAt(shape.i - 1, shape.j) !== WALL) {
+            oldPos = myClone(shape);
             shape.i--;
+        }
+        else if (shape.i - 1 < 0 && board.boardAt(board.getBoard().length - 1, shape.j) !== WALL){
+            // Move Between sides.
+            oldPos = myClone(shape);
+            shape.i = board.getBoard().length - 1;
         }
         pacmanDirection = 'left';
     }
     if (x === 4) {
-        if (shape.i < 9 && board[shape.i + 1][shape.j] !== WALL) {
+        if (shape.i < board.getBoard().length - 1 && board.boardAt(shape.i + 1, shape.j) !== WALL) {
+            oldPos = myClone(shape);
             shape.i++;
+        }
+        else if (shape.i === board.getBoard().length - 1 && board.boardAt(0, shape.j) !== WALL){
+            // Move Between sides.
+            oldPos = myClone(shape);
+            shape.i = 0;
         }
         pacmanDirection = 'right';
     }
-    if (board[shape.i][shape.j] > 100) {
-        score += score - 100 + board[shape.i][shape.j];
+    if (board.boardAt(shape.i, shape.j) > 100) {
+        score += - 100 + board.boardAt(shape.i, shape.j);
     }
-    board[shape.i][shape.j] = 2;
+    // board.getBoard()[shape.i][shape.j] = 2;
     var currentTime = new Date();
     time_elapsed = (currentTime - start_time) / 1000;
-    if (score >= 20 && time_elapsed <= 10) {
-        pac_color = "green";
+    // if (score >= 20 && time_elapsed <= 10) {
+    //     pac_color = "green";
+    // }
+    Draw();
+
+    if (specialSnack.isAlive() && testSpecialSnackHit()){
+        score += 50;
+        specialSnack.eatSnack();
     }
-    if (score === 50) {
-        window.clearInterval(interval);
-        window.alert("Game completed");
-    } else {
-        Draw();
+
+    if(testGhostHit()){
+        lives--;
+        if (lives === 0){
+            window.alert("Oh Snap.. You couldn't stop the Snap\n Everyone is DEAD");
+            Start();
+        }
+        else{
+            window.alert("I guess this is not the reality you win.");
+            score -= 10;
+            setCharactersLocations();
+        }
     }
+
+}
+
+function testGhostHit(){
+    for (var i = 0; i < ghosts.length; i++){
+        var bool_1 = (ghosts[i].getI() === shape.i && ghosts[i].getJ() === shape.j);
+        var bool_2 = oldPos !== null;
+        var bool_3;
+        var bool_4;
+        if (bool_2) {
+            bool_3 = (ghosts[i].getI() === oldPos.i && ghosts[i].getJ() === oldPos.j);
+            bool_4 = (ghosts[i].getOldPosition().i === shape.i && ghosts[i].getOldPosition().j === shape.j);
+        }
+        if ((ghosts[i].getI() === shape.i && ghosts[i].getJ() === shape.j) ||
+            (oldPos !== null && (ghosts[i].getI() === oldPos.i && ghosts[i].getJ() === oldPos.j ) &&
+                (ghosts[i].getOldPosition().i === shape.i && ghosts[i].getOldPosition().j === shape.j))){
+            return true;
+        }
+    }
+    return false;
+}
+
+function testSpecialSnackHit(){
+    if ((specialSnack.getI() === shape.i && specialSnack.getJ() === shape.j) ||
+        (oldPos !== null && (specialSnack.getI() === oldPos.i && specialSnack.getJ() === oldPos.j ) &&
+            (specialSnack.getOldPosition().i === shape.i && specialSnack.getOldPosition().j === shape.j))){
+        return true;
+    }
+    return false;
+}
+
+
+function myClone(src) {
+    return Object.assign({}, src);
 }
